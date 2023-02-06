9cf5b52c10eae4fbba18ee5bfb696f7158c2f229
qurriahSam <mugirethe1@gmail.com>
2023-02-06 16:35:12 +0300

    ch: setup server using express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,6 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home Page</h1>");
-    res.end();
-  } else if (url === "/about") {
-    console.log("server hit");
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About Page</h1>");
-    res.end();
-  } else {
-    console.log("server hit");
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>404 page not found</h1>");
-    res.end();
-  }
+app.listen(4000, () => {
+  console.log("server is listening on port 4000...");
 });
-
-server.listen(4000);
