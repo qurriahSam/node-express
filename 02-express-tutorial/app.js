1242962ca10ac3eeaa383d3609a787932e25fdd3
qurriahSam <mugirethe1@gmail.com>
2023-02-06 17:06:41 +0300

    ft: setup routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -22,7 +22,22 @@
 */
 
 const express = require("express");
+const path = require("path");
 const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("about");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>path not found</h1>");
+});
 
 app.listen(4000, () => {
   console.log("server is listening on port 4000...");
