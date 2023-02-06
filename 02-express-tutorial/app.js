79afdb1b0a948ab59da2259e7bbd3b5cfdb41c2a
qurriahSam <mugirethe1@gmail.com>
2023-02-06 15:17:00 +0300

    create server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+ console.log('server hit');
+ res.end('home')
+})
+
+server.listen(4000)
