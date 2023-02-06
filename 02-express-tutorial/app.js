a78bb98f3d50c6f30f4aabb7ba4340d24a175acc
qurriahSam <mugirethe1@gmail.com>
2023-02-06 15:56:43 +0300

    ft: setup different url responses

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("server hit");
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Home Page</h1>");
-  res.end();
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Home Page</h1>");
+    res.end();
+  } else if (url === "/about") {
+    console.log("server hit");
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About Page</h1>");
+    res.end();
+  } else {
+    console.log("server hit");
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>404 page not found</h1>");
+    res.end();
+  }
 });
 
 server.listen(4000);
