253468673f6d664a4d406ab44765bcd1dd0dc30a
qurriahSam <mugirethe1@gmail.com>
2023-02-06 15:23:04 +0300

    add response data and headers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,10 @@
-const http = require('http')
+const http = require("http");
 
 const server = http.createServer((req, res) => {
- console.log('server hit');
- res.end('home')
-})
+  console.log("server hit");
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>Home Page</h1>");
+  res.end();
+});
 
-server.listen(4000)
+server.listen(4000);
